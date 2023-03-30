623aaa5bddb379656c1b4f060bca96e5a4a9d69d
senapatisachi111 <116190773+senapatisachi111@users.noreply.github.com>
2023-03-30 20:33:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning main Repository"
-git clone https://github.com/Joelkb/The-Fun-Bot.git /The-Fun-Bot
+git clone https://github.com/senapatisachi111/The-Fun-Bot.git /The-Fun-Bot
 cd /The-Fun-Bot
 pip3 install -U -r requirements.txt
 echo "Starting The-Fun-Bot..."
